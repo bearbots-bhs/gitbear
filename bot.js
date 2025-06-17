ee33907934b28c7e40f34ae7e7a4d0e9490a2b80
luke89joseph <luke89joseph@gmail.com>
2025-06-17 17:05:19 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -10,13 +10,12 @@ document.getElementById("linkTO-repo").onclick = function () {
 
 let directory = localStorage.getItem("botDir");
 
-fetch (`https://raw.githubusercontent.com/bearbots-bhs/gitbear/main/imports/${directory}/config.json`)
+fetch (`imports/${directory}/config.json`)
     .then(function(response) {
         return response.json();
     })
     .then (function(data){
         document.getElementById("title").innerHTML = data[0].name;
-        //window.location.href = "https://raw.githubusercontent.com/bearbots-bhs/gitbear/main/imports/intoTheDeep24-25/Acompt_FEB2025.java";
         let content = "";
         for (let i = 0; i < data[1].length; i++) {
 
@@ -24,58 +23,35 @@ fetch (`https://raw.githubusercontent.com/bearbots-bhs/gitbear/main/imports/${di
 
             let id = String(k.name.substring(0, k.name.length-5).trim());
             //console.log(id);
-            let link = `https://raw.githubusercontent.com/bearbots-bhs/gitbear/main/imports/${directory}/${k.name}`;
             content += `
-            <a href = ${link}>
             <div class="script" id="${id}">
                 <h2>${k.name}</h2>
                 <h3>${k.task}</h3>
             </div>
-            </a>
             <br>
             `;
-            /*
-            console.log(id);
-            document.getElementById(id).onclick = function () {
-                window.location.href = `https://raw.githubusercontent.com/bearbots-bhs/gitbear/main/imports/${directory}/${k.name}`;
-            };
-            */
-            //console.log(document.getElementById(id).onclick);
+
         }
         content += `<h1>CREDITS</h1><h4>${data[0].credits}</h4>`;
-        canvas.innerHTML += content;
-
-        /*
+            canvas.innerHTML += content;
         for (let i = 0; i < data[1].length; i++) {
+
             let k = data[1][i];
-            document.getElementById(id).onclick = function () {
-                window.location.href = `https://raw.githubusercontent.com/bearbots-bhs/gitbear/main/imports/${directory}/${k.name}`;
-            };
-        };
-        */
+
+            let id = String(k.name.substring(0, k.name.length-5).trim());
+            //console.log(id);
+            let link = `imports/${directory}/${k.name}`;
+            console.log(id, link);
+
+            document.getElementById(`${id}`).onclick = function() {
+                window.location.href = `${link}`;
+                console.log("ok");
+            }
+        }
+
 
     })
     .catch (function(error){
         console.error("Error fetching json", error);
     });
 
-/*
-    function renderScript(k, data) {
-        let i = data[1][k];
-
-        let id = i.name.substring(0, i.name.length-5)
-        canvas.innerHTML += `
-        <div class="script" id="${id}">
-            <h2>${i.name}</h2>
-            <h3>${i.task}</h4>
-        </div>
-        <br>
-        `
-        document.getElementById(id).onclick = function() {
-            window.location.href = `https://raw.githubusercontent.com/bearbots-bhs/gitbear/main/imports/${directory}/${i.name}`
-        }
-
-        //console.log(id);
-        
-    }
-        */
--- main.js
+++ main.js
@@ -1,60 +1,25 @@
-
-
-
-
-/*
-console.log("start");
-let image = "placeholder.png";
-fetch("https://raw.githubusercontent.com/bearbots-bhs/gitbear/refs/heads/main/imports/config.json")
+fetch (`imports/config.json`)
     .then(function(response) {
-        console.log(response);
-        //return response;
-        return response.json();  // Convert the response to JSON
+        return response.json();
     })
-    .then(function(data) {
+    .then (function(data){
+        let toAdd = "";
         for (let i = 0; i < data.length; i++) {
-            renderBot(data[i].dir);
-            console.log(data[i].dir);
+            toAdd += `
+            <div class="robot" id="${data[i].dir}">
+                <img src="images/${data[i].img}">
+                <h2 class="montserratFONT">${data[i].name}</h2>
+            </div>
+            <br><br>
+            `
         }
-        console.log("done");
+        document.getElementById("mainbody").innerHTML = toAdd;
+
+        for (let i = 0; i < data.length; i++) {
+            document.getElementById(`${data[i].dir}`).onclick = function() {
+                localStorage.setItem("botDir", `${data[i].dir}`);
+                window.location.href = "bot.html";
+            }
+        }
+
     })
-    .catch(function(error) {
-        console.error("Error fetching JSON", error);
-    });
-    
-
-function renderBot(dir) {
-    
-    let url = `https://raw.githubusercontent.com/bearbots-bhs/gitbear/main/imports/${dir}/config.json`;
-
-    fetch(url)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(data) {
-            if (data[0].img != null) {
-                image = data[0].img;
-            }
-            else {
-                image = "placeholder.png";
-            }
-
-            //console.log("k");
-            document.getElementById("mainbody").innerHTML += `
-                <a href="bot.html">
-                <div class="robot" id="${dir}">
-                    <img src="images/${image}">
-                    <h2>${data[0].name}</h2>
-                </div>
-                </a>
-                <br>
-            `;
-            //console.log("o");
-            
-            
-        })
-        .catch(function(error) {
-            console.error("Error fetching JSON", error);
-        });
-}
-*/
